71413926782c8e0dcc06847b88416caf6d39f5ec
Jeff Qiu <zhenfeng.qiu@tufts.edu>
2022-03-26 17:51:05 -0400

    Add time info in server output

--- src/server.py
+++ src/server.py
@@ -1,12 +1,17 @@
+import zoneinfo
 from feed import Feed
 from time import sleep
 from config import SOURCES, SLEEP_TIME
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 if __name__ == "__main__":
     while True:
         for d in SOURCES:
             f = Feed(d["url"], d["source"], d["tags"])
             updated = f.update()
-        print("[Server] Update complete, {} articles updated.".format(updated))
+        now = datetime.now()
+        now.replace(tzinfo=ZoneInfo("America/New_York"))
+        print("[{}] Update complete, {} articles updated.".format(now.strftime("%H:%M:%S"), updated))
         sleep(SLEEP_TIME)
         

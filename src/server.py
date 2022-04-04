dcef119b403c90880b3af453a9878ef7f0880764
Jeff Qiu <zhenfeng.qiu@tufts.edu>
2022-04-04 09:25:33 -0400

    Add update exception handling

--- src/server.py
+++ src/server.py
@@ -4,12 +4,16 @@ from time import sleep
 from config import SOURCES, SLEEP_TIME
 from datetime import datetime
 from zoneinfo import ZoneInfo
+import traceback
 
 if __name__ == "__main__":
     while True:
         for d in SOURCES:
             f = Feed(d["url"], d["source"], d["tags"])
-            updated = f.update()
+            try:
+                updated = f.update()
+            except:
+                traceback.print_exc()
         now = datetime.now()
         now.replace(tzinfo=ZoneInfo("America/New_York"))
         print("[{}] Update complete, {} articles updated.".format(now.strftime("%H:%M:%S"), updated))
